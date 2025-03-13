919125c2ccf8f5c1788517bd003782287cf4c7db
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-03-13 22:04:07 +0200

    Add user input handler function getValue
    Added getValue function to read numeric values ​​from console to avoid code duplication. The function checks input for correctness (whether it is a number) and allows re-entry in case of error. Will be applicable for sequential input of coefficients a, b and c for a quadratic equation.

--- interactiveMode.js
+++ interactiveMode.js
@@ -4,3 +4,15 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+function getValue(name, callback) {
+  rl.question(`Enter ${name}: `, (value) => {
+    const num = Number(value);
+    if (isNaN(num)) {
+      console.log(`Error. Expected a valid number, got '${value}' instead.`);
+      getValue(name, callback);
+    } else {
+      callback(num);
+    }
+  });
+}
