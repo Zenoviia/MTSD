fe80bd29456e0304795fc2b091a9c3654e6e8c22
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-03-13 23:20:40 +0200

    Add function to check if a value is a decimal
    This function uses a regular expression to check if a number is decimal. This is necessary because the task for this lab shows an wxample with hexadecimal number, which causes an error, and this is not the standard behavior of the program.

--- interactiveMode.js
+++ interactiveMode.js
@@ -7,11 +7,17 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+function isDecimal(value) {
+  return /^[-+]?\d+(\.\d+)?$/.test(value);
+}
+
 function getValue(name, callback) {
   rl.question(`Enter ${name}: `, (value) => {
     const num = Number(value);
-    if (isNaN(num)) {
-      console.log(`Error. Expected a valid number, got '${value}' instead.`);
+    if (!isDecimal(value)) {
+      console.log(
+        `Error. Expected a valid real number, got '${value}' instead.`
+      );
       getValue(name, callback);
     } else {
       callback(num);
