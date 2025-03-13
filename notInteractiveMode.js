465a43ce8da1b3341554725e708636c037903014
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-03-13 22:57:53 +0200

    Add file existence check and logic for non-interactive mode
    Implemented fs.access to check if the file exists before reading and processing its contents. If the file is missing, an error message is displayed. Otherwise, the quadratic equation solver processes the data.

--- notInteractiveMode.js
+++ notInteractiveMode.js
@@ -1,8 +1,29 @@
 const quadraticEquationSolver = require("./main.js");
 
+const fs = require("fs");
+
 const filePath = process.argv[2];
 
 if (!filePath) {
   console.log("Error: Please enter a path to the file.");
   process.exit(1);
 }
+
+fs.access(filePath, fs.constants.F_OK, (err) => {
+  if (err) {
+    console.log(`file ${filePath} does not exist`);
+  } else {
+    const fileData = fs.readFileSync(filePath, "utf-8").trim();
+
+    const [aStr, bStr, cStr] = fileData.split(" ");
+    const a = Number(aStr);
+    const b = Number(bStr);
+    const c = Number(cStr);
+
+    const functionCall = quadraticEquationSolver(a, b, c);
+    console.log(
+      `Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0\n`,
+      functionCall
+    );
+  }
+});
