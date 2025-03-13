4860d4fe1cb69e02d7f4966bb04e58dc89f747c3
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-03-13 22:36:10 +0200

    Add check for missing file path argument

--- notInteractiveMode.js
+++ notInteractiveMode.js
@@ -1,3 +1,8 @@
 const quadraticEquationSolver = require("./main.js");
 
 const filePath = process.argv[2];
+
+if (!filePath) {
+  console.log("Error: Please enter a path to the file.");
+  process.exit(1);
+}
