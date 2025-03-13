4a50382a46900db6bf698a81f9b64d1a1adc957b
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-03-13 23:09:26 +0200

    Fix the code

--- notInteractiveMode.js
+++ notInteractiveMode.js
@@ -12,28 +12,31 @@ if (!filePath) {
 fs.access(filePath, fs.constants.F_OK, (err) => {
   if (err) {
     console.log(`file ${filePath} does not exist`);
-  } else {
-    const fileData = fs.readFileSync(filePath, "utf-8").trim();
-
-    const [aStr, bStr, cStr] = fileData.split(" ");
-    const a = Number(aStr);
-    const b = Number(bStr);
-    const c = Number(cStr);
-
-    const functionCall = quadraticEquationSolver(a, b, c);
-    console.log(
-      `Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0\n`,
-      functionCall
-    );
+    return;
   }
+
+  const fileData = fs.readFileSync(filePath, "utf-8").trim();
+
+  const [aStr, bStr, cStr] = fileData.split(" ");
+  const a = Number(aStr);
+  const b = Number(bStr);
+  const c = Number(cStr);
+
+  const values = fileData.split(" ");
+
+  if (values.length !== 3 || !values.every((val) => !isNaN(Number(val)))) {
+    console.log("invalid file format");
+    process.exit(1);
+  }
+
+  if (a === 0) {
+    console.log("Error. a can not be equal to 0");
+    process.exit(1);
+  }
+
+  const functionCall = quadraticEquationSolver(a, b, c);
+  console.log(
+    `Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0\n`,
+    functionCall
+  );
 });
-
-if (a === 0) {
-  console.log("Error. a can not be equal to 0");
-  process.exit(1);
-}
-
-if (values.length !== 3 || !values.every((val) => !isNaN(Number(val)))) {
-  console.log("invalid file format");
-  process.exit(1);
-}
