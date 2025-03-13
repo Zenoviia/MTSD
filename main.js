08ae20f70706ff571936fa30a60feda175301053
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-03-13 13:31:06 +0200

    Add logic for D > 0 case

--- main.js
+++ main.js
@@ -3,5 +3,10 @@
 const quadraticEquationSolver = (a, b, c) => {
   const D = Math.pow(b, 2) - 4 * a * c;
   const sqrtD = Math.sqrt(D);
-};
 
+  if (D > 0) {
+    const x1 = (-b + sqrtD) / (2 * a);
+    const x2 = (-b - sqrtD) / (2 * a);
+    return `There are 2 roots\nx1 = ${x1}\nx2 = ${x2}`;
+  }
+};
