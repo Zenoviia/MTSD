7d17bda5ba71f05c7ae2df800497ad0003a89210
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-03-13 21:44:57 +0200

    Test input prompt for 'a' coefficient

--- interactiveMode.js
+++ interactiveMode.js
@@ -1,6 +1,20 @@
+const quadraticEquationSolver = require("./main.js");
+
 const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+});
+
+rl.question("Enter a: ", (aStr) => {
+  const a = Number(aStr);
+  if (isNaN(a)) {
+    console.log(`Error. Expected a valid number, got '${aStr}' instead.`);
+    rl.close();
+    return;
+  }
+
+  console.log(`You entered a = ${a}`);
+  rl.close();
 });
